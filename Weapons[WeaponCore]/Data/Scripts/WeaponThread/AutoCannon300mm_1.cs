ef2fee18c3be90a2bc95750a6f291ccdebc1556c
TheFerrariMan <57067933+TheFerrariMan@users.noreply.github.com>
2020-03-18 18:30:12 +0100

    Updated balance to UD Weapons Mod V2

--- Weapons[WeaponCore]/Data/Scripts/WeaponThread/AutoCannon300mm_1.cs
+++ Weapons[WeaponCore]/Data/Scripts/WeaponThread/AutoCannon300mm_1.cs
@@ -94,16 +94,16 @@ namespace WeaponThread
                 },
                 Loading = new LoadingDef
                 {
-                    RateOfFire = 60,
+                    RateOfFire = 0.233,
                     BarrelSpinRate = 0, // visual only, 0 disables and uses RateOfFire
                     BarrelsPerShot = 1,
                     TrajectilesPerBarrel = 1, // Number of Trajectiles per barrel per fire event.
                     SkipBarrels = 0,
-                    ReloadTime = 480, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    ReloadTime = 120, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     DelayUntilFire = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     HeatPerShot = 0, //heat generated per shot
                     MaxHeat = 20000, //max heat before weapon enters cooldown (70% of max heat)
-                    Cooldown = .999f, //percent of max heat to be under to start firing again after overheat accepts .2-.95
+                    Cooldown = .95f, //percent of max heat to be under to start firing again after overheat accepts .2-.95
                     HeatSinkRate = 200000, //amount of heat lost per second
                     DegradeRof = false, // progressively lower rate of fire after 80% heat threshold (80% of max heat)
                     ShotsInBurst = 1,
